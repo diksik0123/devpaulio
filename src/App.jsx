a7932dc361c81ae74f907f0a0a92bbec2443320e
Paul Bocalan <113324083+diksik0123@users.noreply.github.com>
2025-05-12 11:39:31 +0800

    add spinner loading and fade up animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,45 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.scss'
+import React, {useEffect, useState} from 'react'
 
 function App() {
 
-  function update(e){
-  var x = e.clientX || e.touches[0].clientX
-  var y = e.clientY || e.touches[0].clientY
+  const [loading, setLoading] = useState(true);
 
-  document.documentElement.style.setProperty('--cursorX', x + 'px')
-  document.documentElement.style.setProperty('--cursorY', y + 'px')
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    //custom cursor
+    function update(e){
+    var x = e.clientX || e.touches[0].clientX
+    var y = e.clientY || e.touches[0].clientY
+
+    document.documentElement.style.setProperty('--cursorX', x + 'px')
+    document.documentElement.style.setProperty('--cursorY', y + 'px')
+    }
+
+    document.addEventListener('mousemove',update)
+    document.addEventListener('touchmove',update)
+
+    return () => {
+      clearTimeout(timer); 
+      document.removeEventListener('mousemove', update);
+      document.removeEventListener('touchmove', update);
+    }
+    });
+
+  if(loading){
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="spinner-border d-flex justify-content-center align-items-center custom-spinner" role="status">
+        </div>
+        <i class="bi bi-code-slash static-icon skeleton"></i>
+      </div>
+    );
   }
-
-  document.addEventListener('mousemove',update)
-  document.addEventListener('touchmove',update)
 
   return (
     <>
